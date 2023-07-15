d7dcbeb34058cd84380ad207d6fbc91a78affb9a
alencshaji <alencshaji@gmail.com>
2023-07-15 20:38:38 +0530

    delte last element function added

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-function buttonclick(val){
-document.getElementById("screen").value=document.getElementById("screen").value+val;
+function buttonclick(val) {
+    var screen = document.getElementById("screen");
+    screen.value = screen.value + val;
+   
 }
 function clearScreen()
 {
@@ -10,4 +12,9 @@ function equalClick()
     var text=document.getElementById("screen").value
     var result=eval(text)
     document.getElementById("screen").value=result;
+}
+function deleteElement() {
+    var screen=document.getElementById("screen")
+    var currentValue = screen.value
+    screen.value = currentValue.slice(0,-1);
 }
